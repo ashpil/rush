4024fa3bbaaa9e03b8e4c7adc6568fcfca34056b
ashpil <shpilenok2001@gmail.com>
2020-07-31 13:59:18 -0600

    main.rs optimizations via /u/NixieTube

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@ use rush::runner::execute;
 use std::io::{stdin, stdout, Write};
 
 fn main() {
+    let mut stdout = stdout();
+    let stdin = stdin();
+    let mut input = String::new();
+
     loop {
         print!("~> ");
-        stdout().flush().unwrap();
+        stdout.flush().unwrap();
 
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
+        input.clear(); // `read_line` appends, not replaces, so need to clear the string first
+        stdin.read_line(&mut input).unwrap();
 
         let lexer = Lexer::new(&input);
 
         let mut parser = Parser::new(lexer); 
         match parser.get() {
             Ok(command) => {
-                // Some helpful debug printing
+                #[cfg(debug_assertions)] // Only include when not built with `--release` flag
                 println!("\u{001b}[34m[Main]\u{001b}[0m Command: {:?}", command);
 
                 execute(command);
